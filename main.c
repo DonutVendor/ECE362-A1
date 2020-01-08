6542fb8ca72cc21e36198ebb95990b479bbe7415
U-DESKTOP-VKFL82C\Morti <hmort2@pdx.edu>
2020-01-08 13:40:34 -0800

    Basic heading added to main.c

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
+/*
+Names: Hannah Mortimer
+	Devon Mickels
+Date: January 08 2020
+Professor: Schubert
+Class: ECE 362
+Assignment: 1 - Arguments/MQG
+*/
+
+
 #include <stdio.h>
 
+
+// Main method that will run and do shit
 int main(int argc, char const *argv[]) {
 
   printf("%d", argc);
